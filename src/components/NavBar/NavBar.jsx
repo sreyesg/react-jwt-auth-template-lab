912ef6f6b9639e1ac449c7365cc2e8c0c01a344b
sreyesg <santiagoreyes.g@gmail.com>
2024-11-17 10:59:37 -0800

    Refactor NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,23 @@
 const NavBar = ( {user} ) => {
     return(
         <>
+        {
+            user ? (
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="">Sign Out</Link></li>
+                    </ul>
+                </nav>
+            ):(
+                <nav>
+                    <ul>
+                        <li><Link to="/signin">Sign In</Link></li>
+                        <li><Link to="/signup">Sign Up</Link></li>
+                    </ul>
+                </nav>
+            )
+        }
             <h1>Hello World!</h1>
         </>
     )
